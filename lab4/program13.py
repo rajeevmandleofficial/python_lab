f756c67ee069481fd65e55f372608ae8689d9e1e
Rajeev <rajeevmandle.official@gmail.com>
2022-11-23 17:53:04 +0530

    program 13 is completed

---
+++ lab4/program13.py
@@ -0,0 +1,7 @@
+# WAP to create a list containing only the String length from each string present in another list of strings. Use list comprehension.
+s = "my name is termux iam an terminal "
+
+l1 = s.split()
+
+l2 = [len(x) for x in l1]
+print("list with lenth of string s for another list :", l2)
