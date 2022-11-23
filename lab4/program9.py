e72f88ffd42cf5a386ee271e4d8e1399b1e68399
Rajeev <rajeevmandle.official@gmail.com>
2022-11-23 16:24:45 +0530

    program9  is completed of lab 4

--- lab4/program9.py
+++ lab4/program9.py
@@ -1,2 +1,5 @@
 # WAP to demonstrate the use of sort() of List class, to sort the elements of the List containing Strings based on string length. (Hint: use ‘key’ parameter)
-#
+
+x = "Rajeev Mandle"
+
+print(sorted(x.split(), reverse=True))
