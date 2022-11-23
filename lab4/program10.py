3a413aa4150a54cf1e6af5c8caea5d8e0076cdda
Rajeev <rajeevmandle.official@gmail.com>
2022-11-23 16:34:06 +0530

    program10 is completed

---
+++ lab4/program10.py
@@ -0,0 +1,4 @@
+"""	WAP to find the greatest list from the following nested list l1=[[10,20,30],[40,50,60],[70,80,90]]"""
+l1 = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+
+print("greatest list is {}".format(max(l1)))
--- lab4/program9.py
+++ lab4/program9.py
@@ -1,5 +1,6 @@
 # WAP to demonstrate the use of sort() of List class, to sort the elements of the List containing Strings based on string length. (Hint: use ‘key’ parameter)
 
-x = "Rajeev Mandle"
+x = input("Enter a string:")
+
 
 print(sorted(x.split(), reverse=True))
