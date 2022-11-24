d646a45cfd600368b74f1f55abf7473e63ef5bf2
Rajeev <rajeevmandle.official@gmail.com>
2022-11-24 12:49:25 +0530

    program6 is completed

--- lab8/program6.py
+++ lab8/program6.py
@@ -1,3 +1,13 @@
 # WAP to input name and age from user. The program should generate an exception if name or age are not in proper format i.e. age should be numeric and name should contain only alphabets.
+class CustomError(Exception):
+    pass
+
+
 name = input("enter your name:")
 age = int(input("enter you age:"))
+if type(age) != int:
+    raise CustomError(
+        "age should be numeric and name should contain only alphabets.")
+elif not (name.isalpha)():
+    raise CustomError(
+        "age should be numeric and name should contain only alphabets.")
